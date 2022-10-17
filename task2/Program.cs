58d90fd18dcdd0f3fd0601998ff5edcc5b8aa591
Luba <bart6891@ya.ru>
2022-10-17 14:33:43 +0300

    sum of numbers

---
+++ task2/Program.cs
@@ -0,0 +1,38 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int number = ReadInt("Введите число: ");
+int length = NumberLength(number);
+SumNumbers(number, length);
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция подсчета цифр в числе
+int NumberLength(int A)
+{
+    int index = 0;
+    while (A > 0)
+    {
+        A /= 10;
+        index++;
+    }
+    return index;
+}
+
+// Функция вывода суммы цифр в числе
+void SumNumbers(int n, int length)
+{
+    int sum = 0;
+    for (int i = 1; i <= length; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
